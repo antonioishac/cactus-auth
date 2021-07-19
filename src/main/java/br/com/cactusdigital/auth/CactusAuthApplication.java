11f7ff80cdb86a23f593ebedf5711b4833bdfa21
Antonio Ishac <antonioishac@gmail.com>
2021-07-19 13:44:08 -0300

    first commit

---
+++ src/main/java/br/com/cactusdigital/auth/CactusAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.cactusdigital.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CactusAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CactusAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cactusdigital/auth/CactusAuthApplicationTests.java
@@ -0,0 +1,5 @@
+package br.com.cactusdigital.auth;
+
+class CactusAuthApplicationTests {
+
+}
